d5e59b560f50bc2199009f19878bb580ba412574
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-07-27 18:09:39 +0200

    Tanuló példák készítésének lehetősége
    Elvégzett feladatok: Elkészült a második nagy rész, mely lehetővé teszi a tanuló példák létrehozását. Ehhez be kell tallózni egy mappát, melyben a tanulandó feladatok vannak és egy cél mappát is, ahová az elkészített tanuló adatokat menteni akarjuk. A projekt ezen részét a cnfStats prjektből emeltem át, ami java nyelven íródott. Ez a rész készít a feladatról egy statisztikát, amit apróbb formázással egy neurális hálózat képes elfogadni bemenő adatként.

--- PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/MainWindow.xaml.cs
+++ PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/MainWindow.xaml.cs
@@ -19,18 +19,40 @@ namespace PROBLEM_CLASSIFIER_W_NEURAL_NETWORK
     public partial class MainWindow : Window
     {
         CreateLearningLabelsControl cllc;
+        CreateLearningSetControl clsc;
+        MakeNeuralNetworkControl mnnc;
+
+        List<string> labels;
 
         public MainWindow()
         {
             InitializeComponent();
-
-
+            labels = new List<string>();
         }
 
         private void btn_learningLabels_Click(object sender, RoutedEventArgs e)
         {
             if (cllc == null) cllc = new CreateLearningLabelsControl();
             userControlHolder.Content = cllc;
+        }
+
+        private void btn_learningSet_Click(object sender, RoutedEventArgs e)
+        {
+            labels = (cllc == null)? null: cllc.GetLabels();
+
+            if (labels != null && labels.Count > 0)
+            {
+                if (clsc == null) clsc = new CreateLearningSetControl();
+                clsc.refreshLabels(this.labels);
+                userControlHolder.Content = clsc;
+            }
+            else MessageBox.Show("You need to set at least one or more label first!");
+        }
+
+        private void btn_makeNeuralNetwork_Click(object sender, RoutedEventArgs e)
+        {
+            if (mnnc == null) mnnc = new MakeNeuralNetworkControl();
+            userControlHolder.Content = mnnc;
         }
     }
 }
