8c6240838db371557450e01666aec40d6ed9cdd3
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-07-29 11:00:21 +0200

    Neurális hálózat létrehozásának lehetősége
    Elvégzett feladatok: Megvalósítottam a harmadik nagy részt, mely lehetővé teszi a tetszőleges neurális hálózat létrehozását. 3 támogatott aktivációs függvény: Sigmoid, TanH, ReLU de ezeket a stratégia tervezési minta miatt könnyű továbbfejeszteni. A hálózatnak könnyű módosítani a réteg illetve neuronszámát. Az elkészült hálózatot az alapértelmezett mentési helyre teszi.

---
+++ PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/Classes/NeuralNetwork/NetworkHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROBLEM_CLASSIFIER_W_NEURAL_NETWORK.Classes.NeuralNetwork
+{
+    class NetworkHelper
+    {
+        public static void SaveNetToFile(NeuralNetwork net, string fileName)
+        {
+            StreamWriter sw = new StreamWriter(fileName);
+
+            for (int i = 0; i < net.Layers.Length; i++)
+            {
+                sw.Write(net.Layers[i].NumberOfInput.ToString());
+                sw.Write(":");
+                if (i == net.Layers.Length - 1) sw.Write(net.Layers[i].NumberOfOutput.ToString());
+            }
+            sw.WriteLine(":" + net.Activation);
+
+            foreach (Layer layer in net.Layers)
+            {
+                sw.WriteLine(layer.NumberOfInput.ToString() + ":" + layer.NumberOfOutput.ToString());
+
+                for (int i = 0; i < layer.Output.Length; i++)
+                {
+                    sw.Write(layer.Output[i].ToString());
+                    if (i < layer.Output.Length - 1) sw.Write(":");
+                }
+                sw.WriteLine();
+
+                for (int i = 0; i < layer.Input.Length; i++)
+                {
+                    sw.Write(layer.Input[i].ToString());
+                    if (i < layer.Input.Length - 1) sw.Write(":");
+                }
+                sw.WriteLine();
+
+                for (int i = 0; i < layer.NumberOfOutput; i++)
+                {
+                    for (int j = 0; j < layer.NumberOfInput; j++)
+                    {
+                        sw.Write(layer.Weights[i, j].ToString());
+                        if (j < layer.NumberOfInput - 1) sw.Write(":");
+                    }
+                    sw.WriteLine();
+                }
+            }
+            sw.Close();
+        }
+
+    }
+}
--- PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/UserControls/MakeNeuralNetworkControl.xaml.cs
+++ PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/PROBLEM-CLASSIFIER-W-NEURAL-NETWORK/UserControls/MakeNeuralNetworkControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PROBLEM_CLASSIFIER_W_NEURAL_NETWORK.Classes.NeuralNetwork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,39 @@ namespace PROBLEM_CLASSIFIER_W_NEURAL_NETWORK.UserControls
         public MakeNeuralNetworkControl()
         {
             InitializeComponent();
+
+            this.ucInputLayer.Content = new NetwokLayerSizeControl();
+            this.ucOutputLayer.Content = new NetwokLayerSizeControl();
+            this.wp_layers.Items.Add(new NetwokLayerSizeControl());
+
+            this.cb_activationFunctions.Items.Add("Sigmoid");
+            this.cb_activationFunctions.Items.Add("TanH");
+            this.cb_activationFunctions.Items.Add("ReLU");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.wp_layers.Items.Add(new NetwokLayerSizeControl());
+        }
+
+        private void btn_createNetwork_Click(object sender, RoutedEventArgs e)
+        {
+            int[] layers = new int[this.wp_layers.Items.Count + 2];
+            layers[0] = Convert.ToInt32((this.ucInputLayer.Content as NetwokLayerSizeControl).GetLayerSize());
+
+            for (int i = 1; i < layers.Length - 1; i++)
+            {
+                layers[i] = Convert.ToInt32((this.wp_layers.Items[i-1] as NetwokLayerSizeControl).GetLayerSize());
+            }
+
+            layers[layers.Length-1] = Convert.ToInt32((this.ucOutputLayer.Content as NetwokLayerSizeControl).GetLayerSize());
+
+            ActivationStrategy acti = new Sigmoid();
+            if (this.cb_activationFunctions.SelectedIndex == 0) acti = new Sigmoid();
+            else if (this.cb_activationFunctions.SelectedIndex == 1) acti = new TanH();
+            else if (this.cb_activationFunctions.SelectedIndex == 2) acti = new ReLU();
+
+            NetworkHelper.SaveNetToFile(new NeuralNetwork(layers, acti), "neuralNetwork.txt"); 
         }
     }
 }
